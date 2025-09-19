b3b8b6c8c7d875459e68be0c1705dbd3e9f546e8
taayyab <140433177+taayyab@users.noreply.github.com>
2025-09-19 17:53:22 +0500

    Update testimonials.tsx

--- components/home/testimonials.tsx
+++ components/home/testimonials.tsx
@@ -98,7 +98,7 @@ export default function TestimonialsSection() {
   };
 
   return (
-    <section className="py-16 bg-gray-50">
+    <section className="py-16 bg-gray-50" id="testimonials">
       <div className="container mx-auto">
         <div className="text-center mb-12">
           <h2 className="text-3xl font-extrabold leading-tight mb-4">
